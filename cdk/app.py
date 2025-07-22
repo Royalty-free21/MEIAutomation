359fb0f863b7ed8d75a4e86f0480d5cb7f29fd2b
Royalty-free21 <145382593+Royalty-free21@users.noreply.github.com>
2025-07-22 11:15:46 +0530

    app.py

---
+++ cdk/app.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import aws_cdk as cdk
+from cdk_stack import InfraStack
+
+app = cdk.App()
+InfraStack(app, "InfraStack", env=cdk.Environment(account="123456789012", region="us-east-1"))
+app.synth()
